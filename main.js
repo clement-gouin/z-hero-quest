98f5a85d115eab596b9849588714dea423e4e1c6
Clément Gouin <clement.gouin@nameshield.net>
2025-03-07 15:28:50 +0100

    wip parsing

--- main.js
+++ main.js
@@ -15,8 +15,16 @@ const HELP_PART_2 = [
 ];
 const HELP_PART_3 = ["Condition (JS eval of variables)", "Data (html)"];
 const DEFAULT_VALUES = {
-  // TODO: 5. implement custom logic
   header: "",
+  namespace: "default",
+  hasNamespace: true,
+  hue: "180",
+  saturation: "30%",
+  hasColor: true,
+  hasVars: false,
+  shownVars: [],
+  hasChanges: false,
+  changes: [],
   data: [],
 };
 
@@ -112,30 +120,43 @@ const app = createApp({
         : "";
     },
     updateEditor(value) {
-      // TODO
       const debugDataSplit = value.split("\n");
-      // let size = HELP_HEADER.length + HELP_PART.length;
-      // while (debugDataSplit.length > size) {
-      //   size += HELP_PART.length;
-      // }
-      // const lines = Array(size).fill(0);
+      const headerSize =
+        HELP_HEADER.length -
+        (this.parsed.hasNamespace ? 0 : 1) -
+        (this.parsed.hasColor ? 0 : 1);
+      const varsSize =
+        1 + (this.parsed.hasVars ? this.parsed.shownVars.length : 1);
+      const changesSize =
+        1 + (this.parsed.hasChanges ? this.parsed.changes.length : 1);
+      let size = headerSize + varsSize + changesSize + HELP_PART_3.length;
+      while (debugDataSplit.length > size) {
+        size += HELP_PART_3.length;
+      }
+      const lines = Array(size).fill(0);
       this.editor.numbersText = debugDataSplit
         .map((_value, index) => `${index + 1}.`)
         .join("\n");
-      // this.editor.overlayText = lines
-      //   .map((_value, index) => {
-      //     if (
-      //       debugDataSplit.length > index &&
-      //       debugDataSplit[index].trim().length
-      //     ) {
-      //       return " ".repeat(debugDataSplit[index].length);
-      //     }
-      //     if (HELP_HEADER.length > index) {
-      //       return HELP_HEADER[index];
-      //     }
-      //     return HELP_PART[(index - HELP_HEADER.length) % HELP_PART.length];
-      //   })
-      //   .join("\n");
+      this.editor.overlayText = lines
+        .map((_value, index) => {
+          if (
+            debugDataSplit.length > index &&
+            debugDataSplit[index].trim().length
+          ) {
+            return " ".repeat(debugDataSplit[index].length);
+          }
+          if (headerSize > index) {
+            return HELP_HEADER[index];
+          } else if (headerSize + varsSize > index) {
+            return HELP_PART_1[Math.min(1, index - headerSize)];
+          } else if (headerSize + varsSize + changesSize > index) {
+            return HELP_PART_2[Math.min(1, index - headerSize - varsSize)];
+          }
+          return HELP_PART_3[
+            (index - headerSize - varsSize - changesSize) % HELP_PART_3.length
+          ];
+        })
+        .join("\n");
       this.editor.numbersCols =
         this.editor.numbersText.length.toString().length + 1;
     },
@@ -145,22 +166,93 @@ const app = createApp({
       this.$refs.overlay.scrollLeft = this.$refs.code.scrollLeft;
     },
     readZData(str) {
-      // TODO: 5. implement custom logic
       this.debugData = str;
       this.parsed = utils.clone(DEFAULT_VALUES);
-      const parts = str.split("\n");
-      if (parts.length < 1) {
+      let parts = str.split("\n");
+      if (!parts.length) {
         return true;
       }
+      parts = this.parseHeader(parts);
+      parts = this.parseVars(parts);
+      parts = this.parseChanges(parts);
+      parts = this.parseData(parts);
+      return parts.length > 0;
+    },
+    parseHeader(parts) {
       this.parsed.header = parts.shift();
       if (!/<[^>]*>/u.test(this.parsed.header)) {
         this.parsed.header = `<h1>${this.parsed.header}</h1>`;
       }
-      this.parsed.data = [];
-      while (parts.length) {
-        this.parsed.data.push(parts.shift());
+      if (!parts.length) {
+        return parts;
       }
-      return false;
+      if (!/^\d+$/u.test(parts[0]) && parts[0].length) {
+        this.parsed.namespace = parts.shift();
+      } else {
+        this.parsed.hasNamespace = parts[0].trim().length === 0;
+      }
+      if (!parts.length) {
+        return parts;
+      }
+      if (!/^\d+$/u.test(parts[0]) && parts[0].length) {
+        const rawPart = parts.shift().split(",");
+        this.parsed.hue = (rawPart[0] ?? "180").trim();
+        this.parsed.saturation = (rawPart[1] ?? "30%").trim();
+      } else {
+        this.parsed.hasColor = parts[0].trim().length === 0;
+      }
+      return parts;
+    },
+    parseVars(parts) {
+      if (!parts.length) {
+        return parts;
+      }
+      this.parsed.hasVars = true;
+      const rawCount = parts.shift();
+      const varCount = /^\d+$/u.test(rawCount) ? parseInt(rawCount, 10) : 0;
+      for (let index = 0; index < varCount; index += 1) {
+        if (!parts.length) {
+          return parts;
+        }
+        const rawPart = parts.shift().split(",");
+        this.parsed.shownVars.push({
+          icon: (rawPart[0] ?? "circle-help").trim(),
+          name: (rawPart[1] ?? "var").trim(),
+          default: parseFloat((rawPart[2] ?? "0").trim()),
+        });
+      }
+      return parts;
+    },
+    parseChanges(parts) {
+      while (parts.length && !/^\d+$/u.test(parts[0])) {
+        parts.shift();
+      }
+      if (!parts.length) {
+        return parts;
+      }
+      this.parsed.hasChanges = true;
+      const rawCount = parts.shift();
+      const changeCount = /^\d+$/u.test(rawCount) ? parseInt(rawCount, 10) : 0;
+      for (let index = 0; index < changeCount; index += 1) {
+        if (!parts.length) {
+          return parts;
+        }
+        const rawPart = parts.shift().split(",");
+        this.parsed.changes.push({
+          value: parseFloat((rawPart[0] ?? "0").trim()),
+          name: (rawPart[1] ?? "var").trim(),
+        });
+      }
+      return parts;
+    },
+    parseData(parts) {
+      while (parts.length >= 2) {
+        this.parsed.data.push({
+          condition: parts.shift(),
+          data: parts.shift(),
+        });
+      }
+      return parts;
     },
   },
 });
